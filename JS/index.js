217e161c7db636c58f829b6dc88755b5d74ac0fd
ciamac-da <ciamac.davoudi@gmail.com>
2022-08-28 10:28:35 +0200

    Verified by Marti4n v1.0.1 it works

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+const body = document.querySelector("#body");
+const dropdown = document.querySelector(".dropdown");
+
+
+const changeColor = () => {
+    let color = dropdown.value;
+    body.style.backgroundColor=color;
+}
+changeColor()
